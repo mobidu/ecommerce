b62d94e9824c69d55bf8c918d152ae53bebff3b6
Saddam Almahali <saddam.almahali@gmail.com>
2018-10-02 13:16:07 +0700

    Perbaikan Referral Ketika User tidak mempunyai upline

--- app/Listeners/TambahPointAffiliate.php
+++ app/Listeners/TambahPointAffiliate.php
@@ -30,19 +30,22 @@ class TambahPointAffiliate
         $order = $event->order->order_detail;
         $customer = $event->order->customer;
         $referral = $customer->referal;
-        $saldo_referral = $referral->saldo ? $referral->saldo : 0;
-        if($order){
-            foreach ($order as $o){
-                $komisi = $o->product->komisi ? $o->product->komisi+$komisi : 0;
+        if($referral){
+            $saldo_referral = $referral->saldo ? $referral->saldo : 0;
+            if($order){
+                foreach ($order as $o){
+                    $komisi = $o->product->komisi ? $o->product->komisi+$komisi : 0;
+                }
+            }
+            $saldo_referral = $saldo_referral + $komisi;
+
+            $referral->saldo = $saldo_referral;
+
+            if($referral->save()){
+                \Log::info('Info Mutasi Saldo Referal : Berhasil Menyimpan mutasi saldo referal untuk Nomor Invoice : '.$event->order->invoice);
             }
         }
-        $saldo_referral = $saldo_referral + $komisi;
 
-        $referral->saldo = $saldo_referral;
-
-        if($referral->save()){
-            \Log::info('Info Mutasi Saldo Referal : Berhasil Menyimpan mutasi saldo referal untuk Nomor Invoice : '.$event->order->invoice);
-        }
 
     }
 }
